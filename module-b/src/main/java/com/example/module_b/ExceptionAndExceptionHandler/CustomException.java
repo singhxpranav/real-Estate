06d66eaf3283863fd1de38408401721afd686344
Shobhit Srivastava <shobhitsrivastava2004@gmail.com>
2025-04-19 20:50:56 +0530

    moduleb

--- module-b/src/main/java/com/example/module_b/ExceptionAndExceptionHandler/CustomException.java
+++ module-b/src/main/java/com/example/module_b/ExceptionAndExceptionHandler/CustomException.java
@@ -1,20 +1,16 @@
 package com.example.module_b.ExceptionAndExceptionHandler;
 
-
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import org.springframework.http.HttpStatus;
+
 
 
 @EqualsAndHashCode(callSuper = true)
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public class CustomException extends RuntimeException {
     private String errorMessage;
-    private HttpStatus status;
     public CustomException(String errorMessage) {
         super(errorMessage);
         this.errorMessage = errorMessage;
